c665d463f5024df9f0311fe7d0d73415104b3359
samuel-bd <samuely@frisseblikken.com>
2022-02-11 14:20:29 +0700

    basic theme toggle

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.scss';
 import { debounce } from 'lodash';
+import useLocalStorage from 'use-local-storage';
 
 function App() {
+  const defaultDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+  const [theme, setTheme] = useLocalStorage(
+    'theme',
+    defaultDark ? 'dark' : 'light'
+  );
+
+  const switchTheme = () => {
+    const newTheme = theme === 'light' ? 'dark' : 'light';
+    setTheme(newTheme);
+    console.log(theme);
+  };
+
   return (
-    <div className='App'>
-      <p>Hello world!</p>
+    <div className='app' data-theme={theme}>
+      <h1>Hello world!</h1>
+      <button onClick={switchTheme}>Howdy</button>
     </div>
   );
 }
